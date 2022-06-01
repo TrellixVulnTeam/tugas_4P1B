f5c594e4828af024a1344f483ec52e1a284d2df5
Rahmat Fajar <rahmatfajar3218@gmail.com>
2022-06-01 11:08:30 +0700

    tugas 5 selesai

---
+++ tugas5.js
@@ -0,0 +1,10 @@
+function stringanipulation(word) {
+    if (word.charAt(0) == 'a', word.charAt(0) == 'i', word.charAt(0) == 'u', word.charAt(0) == 'e', word.charAt(0) == 'o') {
+        console.log(word);
+    } else {
+        console.log(word.substr(1) + word.charAt(0) + 'nyo');
+    }
+}
+
+stringanipulation('ayam') //"ayam"
+stringanipulation('bebek') //"ebekbnyo"
