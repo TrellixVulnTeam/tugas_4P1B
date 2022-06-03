652ea2c6b6c2983cb9eff7394b2a63128d651d66
Rahmat Fajar <rahmatfajar3218@gmail.com>
2022-06-03 09:56:20 +0700

    tugas 7 selesai

---
+++ tugas7.js
@@ -0,0 +1,17 @@
+function weirdMultiply(sentece) {
+    let hasil = ("" + sentece).split("")
+    let satu = 1
+    for (let i = 0; i < hasil.length; i++) {
+        satu *= hasil[i]
+    } 
+    
+    if (satu < 10) {
+        return satu;
+    } else {
+        return weirdMultiply(satu);
+    }
+}
+
+console.log(weirdMultiply(39)) //-> 3*9=27 -> 2*7=14 -> 1*4=4
+console.log(weirdMultiply(999)) // ->9*9*9=729 -> 7*2*9=126 ->1*2*6=12 ->1*2=2
+console.log(weirdMultiply(3)) //3
