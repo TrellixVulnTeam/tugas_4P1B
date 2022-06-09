fd49b78c32c61caaca48a593efeade8e60dfabd7
Rahmat Fajar <rahmatfajar3218@gmail.com>
2022-06-09 10:34:19 +0700

    tugas 12 selesai

--- tugass12.js
+++ tugass12.js
@@ -1,36 +1,60 @@
-console.log('selamat datang di permainan tebak kata, silah kan isi dengan jawaban benar yah!')
-const fs = require('fs')
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: 'jawaban: '
-});
-const bacaData = fs.readFileSync('data.json', 'utf-8')
-var isiData = JSON.parse(bacaData)
-var data = 0
-var back = 1
-console.log(isiData[data].definition)
-rl.prompt();
+if (process.argv[2] == 'data.json') {
+    const process = require('process')
+    console.log(process.argv)
+    console.log('selamat datang di permainan tebak kata, silah kan isi dengan jawaban benar yah!')
+    const fs = require('fs')
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'jawaban: '
+    });
+    const bacaData = fs.readFileSync('data.json', 'utf-8')
+    var isiData = JSON.parse(bacaData)
+    var patokan = isiData
+    // console.log(isiData)
+    var data = 0
+    var back = 1
+    var next = 'skip'
+    var benar = 0
+    console.log(isiData[data].definition)
+    rl.prompt();
 
-rl.on('line', (jawaban) => {
-    if (jawaban.toLowerCase() == isiData[data].term) {
-        console.log('anda beruntung!')
-        data++
-        if (data < isiData.length) {
+    rl.on('line', (jawaban) => {
+        if (jawaban.toLowerCase() == isiData[data].term) {
+            console.log('anda beruntung!')
+            isiData.splice(data, 1)
+            if (data == isiData.length) {
+                data = 0
+
+            }
+            benar++
+            if (isiData.length == 0) {
+                console.log('anda berhasil!')
+                rl.close()
+            }
+            // console.log(data)
+            // console.log(benar)
+            if (0 < isiData.length) {
+                console.log(isiData[data].definition)
+                rl.prompt()
+            }
+
+
+        } else if (jawaban.toLowerCase() == next) {
+            data++
+            
             console.log(isiData[data].definition)
             rl.prompt()
-        }
-        if (data == isiData.length) {
-            console.log('anda berhasil!')
-            rl.close()
-        }
-    } else if (jawaban.toLowerCase() != isiData[data].term) {
-        console.log(`anda kurang beruntung! anda salah ${back} kali, silahkan coba lagi!`)
-        console.log(isiData[data].definition)
-        rl.prompt()
-        return back++
-    }
+        } else if (jawaban.toLowerCase() != isiData[data].term) {
+            console.log(`anda kurang beruntung! anda salah ${back} kali, silahkan coba lagi!`)
+            console.log(isiData[data].definition)
+            back++
+            rl.prompt()
 
+        }
+    }
+    )
+} else {
+    console.log('tolong sertakan nama file untuk inputan soalnya')
 }
-)
