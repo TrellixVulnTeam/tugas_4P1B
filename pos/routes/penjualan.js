d41085a3501121ec93aefb4ff0dd897bff7e6c51
Rahmat Fajar <rahmatfajar3218@gmail.com>
2022-09-21 10:08:12 +0700

    penjualan done

--- pos/routes/penjualan.js
+++ pos/routes/penjualan.js
@@ -7,8 +7,9 @@ module.exports = function (db) {
     router.get('/', async function (req, res, next) {
         try {
             const { rows } = await db.query('SELECT * FROM penjualan');
-            const noInvoice = req.query.noInvoice || rows.length > 0 ? rows[0].no_invoice : '';
+            const noInvoice = req.query.noInvoice ? req.query.noInvoice : rows.length > 0 ? rows[0].no_invoice : '';
             const details = await db.query('SELECT dp.*, b.nama_barang FROM detail_penjualan as dp LEFT JOIN barang as b ON dp.id_barang = b.id_barang WHERE dp.no_invoice = $1 ORDER BY dp.id_detail', [noInvoice]);
+            console.log(req.query.noInvoice, noInvoice)
             res.render('penjualan/list', {
                 currentPage: 'penjualan',
                 rows,
@@ -22,12 +23,25 @@ module.exports = function (db) {
             res.send(e)
         }
     });
-    router.get('/add', async function (req, res) {
+    router.get('/create', async function (req, res) {
         try {
-            const { rows } = await db.query('SELECT id_barang, nama_barang FROM barang')
-            res.render('penjualan/add', {
+            const { rows } = await db.query('INSERT INTO penjualan(total_harga) VALUES(0) returning *')
+            res.redirect(`/penjualan/show/${rows[0].no_invoice}`)
+        }
+        catch {
+            res.send(e)
+        }
+    })
+
+    router.get('/show/:no_invoice', async function (req, res) {
+        try {
+            const penjualan = await db.query('SELECT * FROM penjualan WHERE no_invoice = $1', [req.params.no_invoice])
+            const { rows } = await db.query('SELECT id_barang, nama_barang FROM barang ORDER BY id_barang')
+            res.render('penjualan/form', {
                 currentPage: 'penjualan',
-                barang: rows
+                barang: rows,
+                penjualan: penjualan.rows[0],
+                moment
             })
         }
         catch {
@@ -35,12 +49,32 @@ module.exports = function (db) {
         }
     })
 
-    router.post('/start', async function (req, res) {
+    router.get('/barang/:id_barang', async function (req, res) {
         try {
-            const { rows } = await db.query('INSERT INTO penjualan(total_harga) VALUES(0) returning *')
+            const { rows } = await db.query('SELECT * FROM barang WHERE id_barang = $1', [req.params.id_barang]);
             res.json(rows[0])
         }
-        catch {
+        catch(e) {
+            res.send(e)
+        }
+    })
+
+    router.post('/additem', async function (req, res) {
+        try {
+            const { rows } = await db.query('INSERT INTO detail_penjualan(no_invoice, id_barang, qty)VALUES ($1, $2, $3) returnig *', [req.body.no_invoice, req.body.id_barang, req.body.qty]);
+            res.json(rows[0])
+        }
+        catch(e) {
+            res.send(e)
+        }
+    })
+
+    router.get('/details/:no_invoice', async function (req, res) {
+        try {
+            const {rows} = await db.query('SELECT dp.*, b.nama_barang FROM detail_penjualan as dp LEFT JOIN barang as b ON dp.id_barang = b.id_barang WHERE dp.no_invoice = $1 ORDER BY dp.id_detail', [req.params.no_invoice]);
+            res.json(rows)
+        }
+        catch(e) {
             res.send(e)
         }
     })
